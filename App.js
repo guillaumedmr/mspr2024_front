2d788cec6dd4ba98764b19da587300fbed129ccf
GuillaumeDMR <guillaume@daumur.fr>
2024-01-31 11:32:01 +0100

    Pages created

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, View, Image } from 'react-native';
 import ButtonAuth from './components/global/ButtonAuth';
 import LoginScreen from './screens/auth/loginScreen';
+import SignupScreen from './screens/auth/signupScreen';
 
 const Stack = createStackNavigator();
 
@@ -16,16 +17,13 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const HomeScreen = ({ navigation }) => {
-
-  const handlePress = () => {
-    console.log("Le bouton a été pressé !");
-  };
 
   return (
     <View style={styles.container}>
@@ -36,7 +34,7 @@ const HomeScreen = ({ navigation }) => {
         resizeMode="contain"
       />
       <ButtonAuth onPress={() => navigation.navigate('Login')} titre="LOG IN" color={'#31C48D'} colorText={'#fff'} />
-      <ButtonAuth onPress={handlePress} titre="SIGN UP" color={'#fff'} colorText={'#31C48D'} />
+      <ButtonAuth onPress={() => navigation.navigate('Signup')} titre="SIGN UP" color={'#fff'} colorText={'#31C48D'} />
     </View>
   );
 };
